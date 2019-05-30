763d49e8b922ce123223448a1ec186f38042f65c
jamie <jamie.macdonald@nearix.com>
2019-05-30 10:11:23 +0200

    first commit

---
+++ app/Http/Controllers/TranslatorController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use IanLChapman\PigLatinTranslator\Parser;
+
+
+class TranslatorController extends Controller
+{
+	protected $parser;
+	public function __construct (Parser $parser)
+	{
+		$this->parser = $parser;
+
+	}
+    /**
+     * Translates a string from English in to Pig Latin
+     * @param  string $string The string to be translated
+     * @return string         The result of the translation
+     */
+	public function show($string)
+	{
+		return $this->parser->translate($string);
+	}
+}
---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('api')->get('/{string}', 'TranslatorController@show')->name('traslate');
+
+Route::middleware('api')->get('/', function (Request $request) {
+    return redirect()->route('traslate',['string'=> 'Please enter a string to be translated.']);
+});
